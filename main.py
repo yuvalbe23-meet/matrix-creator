9cdc5c69adfbf29ef6a39b71078024a17ff91391
yuvalbe23-meet <yuvalberkovich11@gmail.com>
2024-09-18 22:46:22 +0100

    added positive assets

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ scaling_factor = 5  # Scale up by a factor of 5
 
 # Define shades of blue
 blue_shades = [
-    (255, 255, 224),  # Light yellow
-    (255, 255, 102),  # Pale yellow
-    (255, 255, 0),    # Yellow
-    (204, 204, 0),    # Dark yellow
-    (153, 153, 0)     # Mustard yellow
+    (211, 211, 211),  # Light grey
+    (169, 169, 169),  # Dark grey
+    (128, 128, 128),  # Grey
+    (105, 105, 105),  # Dim grey
+    (47, 79, 79)      # Dark slate grey
 ]
 
 # Define save paths
 save_paths = [
-    r'C:\\Users\\yuval\Desktop\\img_train\\11-30%\\200%change.png',
-    r'C:\\Users\\yuval\Desktop\\img_train\\11-30%\\100.0-2.png',
-    r'C:\\Users\\yuval\Desktop\\img_train\\11-30%\\0.75-2.png',
-    r'C:\\Users\\yuval\Desktop\\img_train\\11-30%\\0.5-2.png',
-    r'C:\\Users\\yuval\Desktop\\img_train\\11-30%\\0.25-2.png'
+    r'C:\\Users\\yuval\Desktop\\img_train\\201-1000%\\200%change.png',
+    r'C:\\Users\\yuval\Desktop\\img_train\\201-1000%\\100.0-6.png',
+    r'C:\\Users\\yuval\Desktop\\img_train\\201-1000%\\0.75-6.png',
+    r'C:\\Users\\yuval\Desktop\\img_train\\201-1000%\\0.5-6.png',
+    r'C:\\Users\\yuval\Desktop\\img_train\\201-1000%\\0.25-6.png'
 ]
 
 # Generate and save each matrix
